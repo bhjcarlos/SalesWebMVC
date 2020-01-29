105203db523cc96576ec10e35d94871c862879e7
Carlos Oliveira <bhjcarlos@hotmail.com>
2020-01-29 19:46:42 -0300

    Update Seller and custon service exception

---
+++ SalesWebMVC/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException //Exceção personalizada
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
